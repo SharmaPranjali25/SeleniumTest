70ab2ef2462c743ec7afcdce0a9000f6f8efd9f5
Pranjali Sharma <sharmapranjali115@gmail.com>
2026-02-05 18:35:16 +0530

    assertion+annotation

--- src/test/java/DayTwoTestNG/AssertionDemo.java
+++ src/test/java/DayTwoTestNG/AssertionDemo.java
@@ -3,33 +3,41 @@ package DayTwoTestNG;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class AssertionDemo {
+    WebDriver driver;
+    @BeforeTest
+    public void initializeBrowser(){
+       driver=new ChromeDriver();
+    }
     @Test
     public void login() {
-        WebDriver driver= new ChromeDriver();
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
-    String expectedTitle="DEMOQAA";
+        String expectedTitle = "DEMOQAA";
         //String expectedTitle="DEMOQA";
-        String actualTitle= driver.getTitle();
+        String actualTitle = driver.getTitle();
         System.out.println(actualTitle);
 
         //Assertions : Assert class
         // Hard Assertion
-       // Assert.assertEquals(actualTitle,expectedTitle,"Title miss-matched");
+        // Assert.assertEquals(actualTitle,expectedTitle,"Title miss-matched");
 
         //soft assertion
-       String actualText= driver.findElement(By.xpath("//h5[text()='Elements']")).getText();
-       String expectedText="Elements";
-        SoftAssert softassert=new SoftAssert();
-        softassert.assertEquals(actualTitle,expectedTitle,"Title MissMatch");
-
-        driver.quit();
-        softassert.assertEquals(actualText,expectedText,"Element not Present");
-        softassert.assertAll();
+        String actualText = driver.findElement(By.xpath("//h5[text()='Elements']")).getText();
+        String expectedText = "Elements";
+        SoftAssert softassert = new SoftAssert();
+        softassert.assertEquals(actualTitle, expectedTitle, "Title MissMatch");
+        softassert.assertEquals(actualText, expectedText, "Element not Present");
     }
-}
+        //softassert.assertAll();
+        @AfterTest
+        public void teardown(){
+            driver.quit();
+        }
+    }
+
