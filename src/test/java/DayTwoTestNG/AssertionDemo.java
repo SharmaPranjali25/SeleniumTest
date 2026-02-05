c5db9d3a788db74e8e81ad0a10df53befd82c9a4
Pranjali Sharma <sharmapranjali115@gmail.com>
2026-02-05 18:21:56 +0530

    assertions

--- src/test/java/DayTwoTestNG/AssertionDemo.java
+++ src/test/java/DayTwoTestNG/AssertionDemo.java
@@ -1,9 +1,11 @@
 package DayTwoTestNG;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class AssertionDemo {
     @Test
@@ -11,14 +13,23 @@ public class AssertionDemo {
         WebDriver driver= new ChromeDriver();
         driver.get("https://demoqa.com/");
         driver.manage().window().maximize();
-//        String expectedTitle="DEMOQAA";
-        String expectedTitle="DEMOQA";
+    String expectedTitle="DEMOQAA";
+        //String expectedTitle="DEMOQA";
         String actualTitle= driver.getTitle();
         System.out.println(actualTitle);
+
         //Assertions : Assert class
         // Hard Assertion
-        Assert.assertEquals(actualTitle,expectedTitle,"Title miss-matched");
+       // Assert.assertEquals(actualTitle,expectedTitle,"Title miss-matched");
+
+        //soft assertion
+       String actualText= driver.findElement(By.xpath("//h5[text()='Elements']")).getText();
+       String expectedText="Elements";
+        SoftAssert softassert=new SoftAssert();
+        softassert.assertEquals(actualTitle,expectedTitle,"Title MissMatch");
 
         driver.quit();
+        softassert.assertEquals(actualText,expectedText,"Element not Present");
+        softassert.assertAll();
     }
 }
