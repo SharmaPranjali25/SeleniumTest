672aeb7981a6fdf4364ad692096498470f6f2953
Pranjali Sharma <sharmapranjali115@gmail.com>
2026-02-11 00:57:02 +0530

    data provider

---
+++ src/test/java/DayFiveTestNG/DataproviderDemo.java
@@ -0,0 +1,46 @@
+package DayFiveTestNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DataproviderDemo {
+    WebDriver driver;
+
+    @BeforeClass
+    void setUp(){
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+    @Test
+    void testLogin() throws InterruptedException {
+        driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("abc@gmail.com");
+        driver.findElement(By.xpath("//input[@id='input-passowrd']")).sendKeys("test@123");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+        Thread.sleep(2000);
+        boolean status=driver.findElement(By.xpath("//h2[normalize-space()='My Account']")).isDisplayed();
+        if(status==true){
+            driver.findElement(By.xpath("//a[@class='list-group-item'][normalize-space()='Logout']")).click();
+            Assert.assertTrue(true);
+        }
+        else{
+            Assert.fail();
+        }
+
+    }
+    @AfterClass
+    void tearDown(){}
+
+
+
+
+}
